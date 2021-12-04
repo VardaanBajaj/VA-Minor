52e6ff1ec882acd1920237b5a8d3b809f036b69e
VardaanBajaj <vardaanbajaj26@gmail.com>
2021-12-04 15:47:44 +0530

    VA for Minor Project

---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+var express = require('express');
+var app = express();
+const events = require('events');
+var socket = require('socket.io');
+//var ytaudio = require('./audioStream');
+var server = app.listen(3030,function(){
+    console.log('Server running on port 3030');
+});
+
+app.use(express.static('public'));
+const linkEmitter = new events.EventEmitter();
+function callPython(message, sendData){
+    // console.log('Inside Python');
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',['./get_news.py', message]);
+    // console.log('Still There');
+    process.stdout.on('data', function(data) {
+        //console.log(data.toString());
+        console.log(data);
+        // console.log(data.toString('utf8'));
+        linkEmitter.emit('link-received', data);
+
+    } );
+    // console.log('Leaving');
+}
+var io = socket(server);
+io.on('connection', function(socket){
+   console.log(`made socket connection ${socket.id}`);
+   socket.on('chat', function(data){
+       var message = data.message.toString();
+       if((message.toString()).lastIndexOf(';;')===0){
+           console.log('Bot Command');
+           callPython(message.slice(2),data);
+
+           //console.log('It emitted')
+       }
+       io.sockets.emit('chat', data);
+   });
+   socket.on('typing', function(data){
+       //console.log(data);
+       socket.broadcast.emit('typing', data);
+   });
+    linkEmitter.on('link-received', (link)=> {
+        console.log(link.toString());
+        link = link.toString();
+        //ytaudio()
+       const res ={message: link,
+       handle: 'bot'};
+        //taudio.getaudio(link,);
+       io.sockets.emit('chat', res);
+    });
+});
